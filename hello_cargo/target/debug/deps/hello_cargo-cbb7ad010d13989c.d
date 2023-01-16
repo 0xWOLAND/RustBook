fe34bc091a9cd50d57c845a92e533c40bdf25e76
chndr1 <bhargav.annem@gmail.com>
2023-01-16 08:43:55 -0600

    hello_cargo & hello_world

---
+++ hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d
@@ -0,0 +1,5 @@
+/home/chndr1/rust_projects/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c: src/main.rs
+
+/home/chndr1/rust_projects/hello_cargo/target/debug/deps/hello_cargo-cbb7ad010d13989c.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/chndr1/rust_projects/hello_cargo/target/debug/hello_cargo: /home/chndr1/rust_projects/hello_cargo/src/main.rs
